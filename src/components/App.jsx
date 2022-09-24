29dbf38a17728221e76de76e6023a0a2370d0856
Konstantin7898 <lexus7898@gmail.com>
2022-09-24 23:11:42 +0300

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { Profile } from 'components/profile/Profile';
+import users from '../components/user.json';
+
 export const App = () => {
+  console.log(users);
+
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+    // style={{
+    //   height: '100vh',
+    //   display: 'flex',
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   fontSize: 40,
+    //   color: '#010101',
+    // }}
     >
-      React homework template
+      {/* <Profile user={users[0]} /> */}
+      <Profile
+        username={users.username}
+        tag={users.tag}
+        location={users.location}
+        avatar={users.avatar}
+        stats={users.stats}
+      />
     </div>
   );
 };
